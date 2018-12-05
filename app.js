fd1f8920df1890d3acbd8e76d15cc49ea4f420d1
sarut yentakham <>
2018-12-05 13:26:34 +0700

    debug

--- app.js
+++ app.js
@@ -15,11 +15,18 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
+    let body;
     // aimlParser.getResult('Hello', (answer, wildCardArray, input) => {
     //     reply(reply_token, input + "-------->" + answer)
     // })
 
-    reply(reply_token, msg)
+    if (msg == "news") {
+        body.push(messageNews());
+    } else {
+        body.push(messageText());
+    }
+
+    reply(reply_token, body)
     res.sendStatus(200)
 })
 
@@ -33,10 +40,7 @@ function reply(reply_token, msg) {
 
     let body = JSON.stringify({
         replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: msg
-        }]
+        messages: msg
     })
 
     request.post({
@@ -46,4 +50,432 @@ function reply(reply_token, msg) {
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode);
     });
+}
+
+
+
+function messageNews() {
+
+    const msg = {
+        "type": "flex",
+        "altText": "Flex Message",
+        "contents": {
+          "type": "carousel",
+          "contents": [
+            {
+              "type": "bubble",
+              "header": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "NEWS",
+                    "size": "sm",
+                    "weight": "bold",
+                    "color": "#AAAAAA"
+                  }
+                ]
+              },
+              "hero": {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover",
+                "action": {
+                  "type": "uri",
+                  "label": "Action",
+                  "uri": "https://linecorp.com/"
+                }
+              },
+              "body": {
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "md",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "flex": 1,
+                    "contents": [
+                      {
+                        "type": "image",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png",
+                        "gravity": "bottom",
+                        "size": "sm",
+                        "aspectRatio": "4:3",
+                        "aspectMode": "cover"
+                      },
+                      {
+                        "type": "image",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_2.png",
+                        "margin": "md",
+                        "size": "sm",
+                        "aspectRatio": "4:3",
+                        "aspectMode": "cover"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "flex": 2,
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "7 Things to Know for Today",
+                        "flex": 1,
+                        "size": "xs",
+                        "gravity": "top"
+                      },
+                      {
+                        "type": "separator"
+                      },
+                      {
+                        "type": "text",
+                        "text": "Hay fever goes wild",
+                        "flex": 2,
+                        "size": "xs",
+                        "gravity": "center"
+                      },
+                      {
+                        "type": "separator"
+                      },
+                      {
+                        "type": "text",
+                        "text": "LINE Pay Begins Barcode Payment Service",
+                        "flex": 2,
+                        "size": "xs",
+                        "gravity": "center"
+                      },
+                      {
+                        "type": "separator"
+                      },
+                      {
+                        "type": "text",
+                        "text": "LINE Adds LINE Wallet",
+                        "flex": 1,
+                        "size": "xs",
+                        "gravity": "bottom"
+                      }
+                    ]
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "More",
+                      "uri": "https://linecorp.com"
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://www.isranews.org/images/2017/isranews/05/010617_ptt.jpg",
+                "align": "center",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "md",
+                "action": {
+                  "type": "uri",
+                  "label": "Action",
+                  "uri": "https://linecorp.com"
+                },
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "PTT Stock",
+                    "size": "xl",
+                    "weight": "bold"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Price : $10.5",
+                            "flex": 0,
+                            "margin": "sm",
+                            "weight": "bold"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "Description:"
+                      },
+                      {
+                        "type": "text",
+                        "text": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                        "margin": "lg",
+                        "size": "sm",
+                        "align": "start",
+                        "gravity": "center",
+                        "weight": "regular",
+                        "wrap": true
+                      }
+                    ]
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "spacer",
+                    "size": "xxl"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Interested",
+                      "uri": "https://linecorp.com"
+                    },
+                    "color": "#905C44",
+                    "style": "primary"
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://www.isranews.org/images/2017/isranews/05/010617_ptt.jpg",
+                "align": "center",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "md",
+                "action": {
+                  "type": "uri",
+                  "label": "Action",
+                  "uri": "https://linecorp.com"
+                },
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "PTT Stock",
+                    "size": "xl",
+                    "weight": "bold"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Price : $10.5",
+                            "flex": 0,
+                            "margin": "sm",
+                            "weight": "bold"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "Description:"
+                      },
+                      {
+                        "type": "text",
+                        "text": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                        "margin": "lg",
+                        "size": "sm",
+                        "align": "start",
+                        "gravity": "center",
+                        "weight": "regular",
+                        "wrap": true
+                      }
+                    ]
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "spacer",
+                    "size": "xxl"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Interested",
+                      "uri": "https://linecorp.com"
+                    },
+                    "color": "#905C44",
+                    "style": "primary"
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://www.isranews.org/images/2017/isranews/05/010617_ptt.jpg",
+                "align": "center",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "md",
+                "action": {
+                  "type": "uri",
+                  "label": "Action",
+                  "uri": "https://linecorp.com"
+                },
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "PTT Stock",
+                    "size": "xl",
+                    "weight": "bold"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Price : $10.5",
+                            "flex": 0,
+                            "margin": "sm",
+                            "weight": "bold"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "Description:"
+                      },
+                      {
+                        "type": "text",
+                        "text": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                        "margin": "lg",
+                        "size": "sm",
+                        "align": "start",
+                        "gravity": "center",
+                        "weight": "regular",
+                        "wrap": true
+                      }
+                    ]
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "spacer",
+                    "size": "xxl"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Interested",
+                      "uri": "https://linecorp.com"
+                    },
+                    "color": "#905C44",
+                    "style": "primary"
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "See more",
+                      "uri": "https://linecorp.com"
+                    },
+                    "flex": 1,
+                    "gravity": "center"
+                  }
+                ]
+              }
+            }
+          ]
+        }
+    };
+    return msg;
+}
+
+function messageText() {
+    const msg = {
+        type: 'text',
+        text: msg
+    };
+
+    return msg;
 }
