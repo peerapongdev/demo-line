ec015599223929414f263398db794f7620998032
sarut yentakham <>
2018-12-05 11:07:04 +0700

    change

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Reply with two static messages
-const token = "SXacpAKQELBJCyS7RfhIpXs0Wd6wgk1plVX5ox1OKMywptsjpzp+uCusljlwn7A9F8b/kxXcmHoTIx7bsJuo4+oLarFGBKmyhF6aHmW1B5ng4Y6sLrpJWi/RI8cv3msPEFRoxx6i9CZBBRFRK6NRUgdB04t89/1O/w1cDnyilFU=";
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -7,16 +6,20 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    reply(reply_token, JSON.stringify(req.body.events))
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+
+
+
+function reply(reply_token, address) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        'Authorization': 'Bearer SXacpAKQELBJCyS7RfhIpXs0Wd6wgk1plVX5ox1OKMywptsjpzp+uCusljlwn7A9F8b/kxXcmHoTIx7bsJuo4+oLarFGBKmyhF6aHmW1B5ng4Y6sLrpJWi/RI8cv3msPEFRoxx6i9CZBBRFRK6NRUgdB04t89/1O/w1cDnyilFU='
     }
     let body = JSON.stringify({
         replyToken: reply_token,
@@ -27,6 +30,10 @@ function reply(reply_token) {
         {
             type: 'text',
             text: 'How are you?'
+        },
+        {
+            type: 'text',
+            text: address
         }]
     })
     request.post({
